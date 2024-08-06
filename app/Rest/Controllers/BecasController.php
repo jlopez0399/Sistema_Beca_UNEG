df0384e0a3bff14503808b7b817ad9e50c79390a
Julián <julianlopezcastillo12@gmail.com>
2024-08-06 15:58:38 -0400

    YA

--- app/Rest/Controllers/BecasController.php
+++ app/Rest/Controllers/BecasController.php
@@ -2,6 +2,7 @@
 
 namespace App\Rest\Controllers;
 
+use App\Models\Beca;
 use App\Rest\Controller as RestController;
 use App\Rest\Resources\BecaResource;
 
@@ -13,4 +14,11 @@ class BecasController extends RestController
      * @var class-string<\Lomkit\Rest\Http\Resource>
      */
     public static $resource = BecaResource::class;
+
+    public function index()
+    {
+        $Becas = Beca::all();
+        return view('Becas.Index', compact('Becas'));
+
+    }
 }
---
+++ resources/views/Becas/Index.blade.php
@@ -0,0 +1,1 @@
+<h1>hola vas bien</h1>
