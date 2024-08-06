37ba74c835cbaa732e6f9a81252effaa88a0bd33
Julián <julianlopezcastillo12@gmail.com>
2024-08-06 17:28:06 -0400

    su

--- resources/views/Becas/Index.blade.php
+++ resources/views/Becas/Index.blade.php
@@ -1,18 +1,85 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Registro de becas') }}
-        </h2>
-    </x-slot>
+<x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{ __('Lista de becas') }}
+    </h2>
+</x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6 lg:p-8">
+            <div class="bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
 
-                <div>
-                    <a href="{{route('Becas.create')}}">Registrar beca</a>
+                <div class="mb-4">
+                    <a href="{{ route('beca.create') }}"
+                        class="bg-cyan-500 dark:bg-cyan-700 hover:bg-cyan-600 dark:hover:bg-cyan-800 text-white font-bold py-2 px-4 rounded">Registrar
+                        beca</a>
                 </div>
+
+                <table class="table-auto w-full">
+                    <thead>
+                        <tr>
+                            <th class="px-4 py-2 text-gray-900 dark:text-white text-center">ID</th>
+                            <th class="px-4 py-2 text-gray-900 dark:text-white text-center">Institución</th>
+                            <th class="px-4 py-2 text-gray-900 dark:text-white text-center">Tipo</th>
+                            <th class="px-4 py-2 text-gray-900 dark:text-white text-center">Accion</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($becas as $beca)
+                            <tr>
+                                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">
+                                    {{ $beca->id }}</td>
+                                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">
+                                    {{ $beca->Intitutio_id }}</td>
+                                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">
+                                    {{ $beca->Type }}</td>
+
+                                <td class="border px-4 py-2 text-center">
+
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
             </div>
         </div>
     </div>
+</div>
 </x-app-layout>
+
+<script>
+    // forma 1
+    function confirmDelete(id) {
+        alertify.confirm("¿Confirm delete record?",
+            function() {
+                let form = document.createElement('form');
+                form.method = 'POST';
+                form.action = '/students/' + id;
+                form.innerHTML = '@csrf @method('DELETE')';
+                document.body.appendChild(form);
+                form.submit();
+                alertify.success('Ok');
+            },
+            function() {
+                alertify.error('Cancel');
+            });
+    }
+
+    // forma 2
+    /* function confirmDelete(id) {
+    alertify.confirm("¿Confirm delete record?", function (e) {
+        if (e) {
+            let form = document.createElement('form');
+            form.method = 'POST';
+            form.action = '/students/' + id;
+            form.innerHTML = '@csrf @method('DELETE')';
+            document.body.appendChild(form);
+            form.submit();
+        } else {
+            alertify.error('Cancel');
+            return false;
+        }
+    });
+    } */
+</script>
